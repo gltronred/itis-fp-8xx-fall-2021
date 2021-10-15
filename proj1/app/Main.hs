6cd74b07cd9d76833adab9a5e24fe75f90e02239
Mansur Ziiatdinov <gltronred@gmail.com>
2021-10-15 17:59:52 +0300

    Project 1 - Transformers

---
+++ proj1/app/Main.hs
@@ -0,0 +1,45 @@
+module Main where
+
+import Control.Monad.Trans.Class
+import Control.Monad.Trans.Reader
+import Data.Maybe
+
+data Expr
+  = Add Expr Expr
+  | Sub Expr Expr
+  | Mul Expr Expr
+  | Div Expr Expr
+  | Number Int
+  | Var String
+  deriving (Eq,Show,Read)
+
+type Env = [(String, Int)]
+
+eval :: Expr -> ReaderT Env Maybe Int
+eval expr = case expr of
+  Add e1 e2 -> do
+    v1 <- eval e1
+    v2 <- eval e2
+    pure $ v1 + v2
+  Sub e1 e2 -> do
+    v1 <- eval e1
+    v2 <- eval e2
+    pure $ v1 - v2
+  Mul e1 e2 -> do
+    v1 <- eval e1
+    v2 <- eval e2
+    pure $ v1 * v2
+  Div e1 e2 -> do
+    v1 <- eval e1
+    v2 <- eval e2
+    pure $ v1 `div` v2
+  Number n -> pure n
+  Var name -> do
+    env <- ask
+    lift $ lookup name env
+
+main :: IO ()
+main = do
+  let expr = Add (Var "x") (Number 2)
+  print $ runReaderT (eval expr) []
+  print $ runReaderT (eval expr) [("x", 2)]
