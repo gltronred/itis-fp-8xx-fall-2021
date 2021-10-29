7513ed349e57961a2202bd1a458b8822b32ff3cf
Mansur Ziiatdinov <gltronred@gmail.com>
2021-10-29 17:13:52 +0300

    Код для stm

--- proj2/app/Main.hs
+++ proj2/app/Main.hs
@@ -2,6 +2,8 @@ module Main where
 
 import Control.Concurrent
 import Control.Concurrent.Async
+import Control.Concurrent.STM
+import Control.Monad
 
 data D = D Integer Integer
 
@@ -31,8 +33,8 @@ list = [ 66333293675293
        , 34298041863719
        ]
 
-main :: IO ()
-main = do
+main1 :: IO ()
+main1 = do
   getNumCapabilities >>= print
   let x = list!!0
       y = list!!1
@@ -47,3 +49,34 @@ main = do
     <*> Concurrently (pp y)
   print a
   print b
+
+summator :: TBQueue Int -> TVar Bool -> Int -> IO Int
+summator q b s = do
+  mx <- atomically $ do
+    finished <- readTVar b
+    if finished
+      then tryReadTBQueue q
+      else Just <$> readTBQueue q
+  case mx of
+    Just x -> summator q b $! s+x
+    Nothing -> pure s
+
+fillQueue :: TBQueue Int -> TVar Bool -> IO ()
+fillQueue q b = do
+  let n = 10000000
+  forM_ [1..n] $ \i ->
+    atomically $ writeTBQueue q i
+  atomically $ writeTVar b True
+
+main = do
+  threads <- getNumCapabilities
+  print threads
+  (q,b) <- atomically $ (,)
+    <$> newTBQueue 10000000
+    <*> newTVar False
+  withAsync (fillQueue q b) $ \r -> do
+    results <- replicateConcurrently threads $
+      summator q b 0
+    wait r
+    forM_ results print
+    print $ sum results
