b9f298b2b0f7d7921d09f9e8700f03ff31f22eb4
PrepConcede <tagmirs_letter@mail.ru>
2021-10-31 12:01:15 +0300

    research

--- du/app/Main.hs
+++ du/app/Main.hs
@@ -1,4 +1,15 @@
 module Main where
+import Control.Monad.Writer.Lazy (WriterT, forM, filterM, MonadTrans (lift))
+import System.Directory (listDirectory, doesDirectoryExist, getFileSize, getCurrentDirectory)
 
-main :: IO ()
-main = putStrLn "Hello, Haskell!"
+
+getDirectories :: FilePath -> IO [FilePath]
+getDirectories currentPath = do
+    allFiles <- listDirectory currentPath
+    filterM doesDirectoryExist allFiles
+
+main = do
+    path <- getCurrentDirectory 
+    print path
+    size <- getFileSize path
+    print size
