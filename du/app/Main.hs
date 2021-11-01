04b0052ca8f91c4f55cc580c41ca27f5cc8daa0c
Tagmir Gilyazov <t1g2r3mr@gmail.com>
2021-11-01 23:42:23 +0300

    exceptions handling

--- du/app/Main.hs
+++ du/app/Main.hs
@@ -2,13 +2,20 @@
 
 module Main where
 
+import Control.Exception.Base
 import Control.Monad.State
-import System.Directory (doesDirectoryExist, getCurrentDirectory, getFileSize, listDirectory)
+import System.Directory
 
 getDirectoriesInSpecifiedFolder :: FilePath -> IO [FilePath]
 getDirectoriesInSpecifiedFolder currentPath = do
-  allFiles <- listDirectory currentPath
-  filterM doesDirectoryExist (map (\fp -> currentPath ++ "/" ++ fp) allFiles)
+  allFilesResult <- try (listDirectory currentPath) :: IO (Either SomeException [FilePath])
+  case allFilesResult of
+    Left se ->
+      ( do
+          putStrLn $ "Exception: " ++ show se
+          return []
+      )
+    Right files -> filterM doesDirectoryExist (map (\fp -> currentPath ++ "/" ++ fp) files)
 
 getFileSizeStr :: FilePath -> IO String
 getFileSizeStr path = do
@@ -17,8 +24,14 @@ getFileSizeStr path = do
 
 getObjectCount :: FilePath -> IO String
 getObjectCount path = do
-  v <- listDirectory path
-  return $ path ++ "\t" ++ show (length v)
+  filesResult <- try (listDirectory path) :: IO (Either SomeException [FilePath])
+  case filesResult of
+    Left se ->
+      ( do
+          putStrLn $ "Exception: " ++ show se
+          return $ path ++ "\t" ++ "(Unable to get elements count)"
+      )
+    Right files -> return $ path ++ "\t" ++ show (length files)
 
 getDirectoriesByDepth :: FilePath -> Int -> StateT [FilePath] IO Bool
 getDirectoriesByDepth path 0 = do
@@ -27,21 +40,21 @@ getDirectoriesByDepth path 0 = do
 getDirectoriesByDepth path depth = do
   modify (path :)
   dirs <- liftIO $ getDirectoriesInSpecifiedFolder path
-  forM_ dirs (\dir -> getDirectoriesByDepth dir (depth -1))
+  forM_ dirs (\dir -> getDirectoriesByDepth dir (depth - 1))
   return True
 
 duHelper :: FilePath -> Int -> IO ()
 duHelper path depth = do
-    folders <- runStateT (getDirectoriesByDepth path depth) []
-    counts <- forM (snd folders) getObjectCount
-    sizes <- forM (snd folders) getFileSizeStr
-    putStrLn "Count:"
-    forM_ counts putStrLn
-    putStrLn "Size:"
-    forM_ sizes putStrLn
+  folders <- runStateT (getDirectoriesByDepth path depth) []
+  counts <- forM (snd folders) getObjectCount
+  sizes <- forM (snd folders) getFileSizeStr
+  putStrLn "Count:"
+  forM_ (reverse counts) putStrLn
+  putStrLn "Size:"
+  forM_ (reverse sizes) putStrLn
 
 du :: FilePath -> Maybe Int -> IO ()
-du path Nothing = duHelper path (maxBound:: Int)
+du path Nothing = duHelper path (maxBound :: Int)
 du path (Just depth) = duHelper path depth
 
-main = du "." (Just 1)
+main = du "." (Just 2)
