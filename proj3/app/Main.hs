b40560fae8df1f4072d682f5726e65187c62e230
Mansur Ziiatdinov <gltronred@gmail.com>
2021-11-05 17:26:25 +0300

    Задание и примеры для него

--- proj3/app/Main.hs
+++ proj3/app/Main.hs
@@ -98,5 +98,16 @@ treeParser = do
 xmlParser :: Parser XML
 xmlParser = many $ try treeParser
 
+csv :: String
+csv = "col1,col2,col3\nr2 c1,r2 c2,r2 c3\n\"r3,c1\",\"r3,c2\",\"r3,c3\"\n\"r4\\\",\\\"\\\\c1\",\"r4\\\",\\\"c2\",\"r4\\\",\\\"c3\""
+
+csvRes :: [[String]]
+csvRes =
+  [ [ "col1", "col2", "col3" ]
+  , [ "r2 c1", "r2 c2", "r2 c3" ]
+  , [ "r3,c1", "r3,c2", "r3,c3" ]
+  , [ "r4\",\"\\c1", "r4\",\"c2", "r4\",\"c3"]
+  ]
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = pure ()
