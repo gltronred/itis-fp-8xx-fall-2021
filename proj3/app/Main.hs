6d5a21c6b88de27af9ef351328068225b156ed56
Mansur Ziiatdinov <gltronred@gmail.com>
2021-11-05 16:00:13 +0300

    Типы данных

--- proj3/app/Main.hs
+++ proj3/app/Main.hs
@@ -1,6 +1,27 @@
 module Main where
 
+import Text.Megaparsec
+
+type Parser = Parsec Void String
+
 -- scheme:[//[user:password@]host[:port]][/]path
+data URL = URL
+  { scheme :: Scheme
+  , domain :: Maybe Domain
+  , path :: String
+  } deriving (Eq,Show)
+
+data Domain = Domain
+  { credentials :: Maybe (String, String)
+  , host :: String
+  , port :: Maybe Int
+  } deriving (Eq,Show)
+
+data Scheme
+  = SchemeHttp
+  | SchemeHttps
+  | SchemeData
+  deriving (Eq,Show)
 
 -- http://example.com
 -- data://asdasdasdads
