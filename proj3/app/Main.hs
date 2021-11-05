3d8afe38a31b65723663723fcef3123a99016853
Mansur Ziiatdinov <gltronred@gmail.com>
2021-11-05 15:54:17 +0300

    Заготовка проекта

---
+++ proj3/app/Main.hs
@@ -0,0 +1,10 @@
+module Main where
+
+-- scheme:[//[user:password@]host[:port]][/]path
+
+-- http://example.com
+-- data://asdasdasdads
+-- https://user:pass@example.com:1234/asd
+
+main :: IO ()
+main = putStrLn "Hello, Haskell!"
