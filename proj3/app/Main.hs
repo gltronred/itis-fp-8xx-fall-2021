7cf855ec6a469cc2e7f338f4de2636d6d943aece
Mansur Ziiatdinov <gltronred@gmail.com>
2021-11-05 16:32:26 +0300

    Парсинг

--- proj3/app/Main.hs
+++ proj3/app/Main.hs
@@ -1,10 +1,13 @@
 module Main where
 
+import Data.Void
 import Text.Megaparsec
+import Text.Megaparsec.Char
 
 type Parser = Parsec Void String
 
 -- scheme:[//[user:password@]host[:port]][/]path
+
 data URL = URL
   { scheme :: Scheme
   , domain :: Maybe Domain
@@ -26,6 +29,37 @@ data Scheme
 -- http://example.com
 -- data://asdasdasdads
 -- https://user:pass@example.com:1234/asd
+
+schemeParser :: Parser Scheme
+schemeParser
+  =   SchemeHttps <$ string "https:"
+  <|> SchemeHttp  <$ string "http:"
+  <|> SchemeData  <$ string "data:"
+
+domainParser :: Parser Domain
+domainParser = do
+  _ <- string "//"
+  mcreds <- optional $ try $ do
+    user <- some alphaNumChar
+    _ <- single ':'
+    pass <- some alphaNumChar
+    _ <- single '@'
+    pure (user, pass)
+  host <- some (alphaNumChar <|> single '.')
+  -- mport <- optional $ do
+  --   _ <- single ':'
+  --   read <$> some digitChar
+  mport <- optional $
+    single ':' *>
+    fmap read (some digitChar)
+  pure $ Domain mcreds host mport
+
+urlParser :: Parser URL
+urlParser = URL
+  <$> schemeParser
+  <*> optional domainParser
+  <*  optional (single '/')
+  <*> takeRest
 
 main :: IO ()
 main = putStrLn "Hello, Haskell!"
