283146e364896eae1e9a4665b0ed5f24ebfa8bdd
Mansur Ziiatdinov <gltronred@gmail.com>
2021-11-12 16:17:05 +0300

    Разбор контрольной: 3

--- cw/app/Main.hs
+++ cw/app/Main.hs
@@ -26,5 +26,34 @@ sol1 = concatMap toResult .
        zip [1..] .
        sortBy (compare `on` negate . snd)
 
+----------------------------------
+
+data Result = Result
+  { teamA :: String
+  , teamB :: String
+  , resA :: Int
+  , resB :: Int
+  } deriving (Eq,Show,Read)
+
+addResult :: [(String,Int)]
+          -> Result
+          -> [(String,Int)]
+addResult state0 r = let
+  (rA,rB) = case compare (resA r) (resB r) of
+    LT -> (0,3)
+    EQ -> (1,1)
+    GT -> (3,0)
+  tA = teamA r
+  tB = teamB r
+  add t r list = case lookup t list of
+    Nothing -> (t,r) : list
+    Just r0 -> (t,r0+r) : delete (t,r0) list
+  in add tA rA $ add tB rB state0
+
+sol3 :: [Result]
+     -> [(String, Int)]
+sol3 = foldl' addResult initState
+  where initState = []
+
 main :: IO ()
 main = putStrLn "Hello, Haskell!"
