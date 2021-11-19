f40f0f5ef9e933bd20b3104336bd87d5a018f6a2
Aydar <ajdarshaydullin@gmail.ru>
2021-11-19 21:57:52 +0300

    add hw1

--- du/app/Main.hs
+++ du/app/Main.hs
@@ -1,4 +1,76 @@
 module Main where
 
-main :: IO ()
-main = putStrLn "Hello, Haskell!"
+import System.Environment
+import System.Directory
+import Data.List
+import Data.Char
+import Control.Monad
+import Control.Monad.State
+import Control.Exception.Base
+import Control.Monad.Except
+import Data.Typeable
+
+dirCount dir = do
+	files <- ls dir
+	return (length files)
+
+dirSize dir = do
+	files <- ls dir
+	fmap sum $ sequence $ map liftIO $ map getFileSize files
+
+allDirsByDepth :: FilePath -> Int -> StateT [(FilePath,(Int,Integer))] IO ()
+
+allDirsByDepth dir 0 = do
+  st <- get	
+  count <- liftIO $ dirCount dir
+  size <- liftIO $ dirSize dir
+  put ((dir, (count, size)) : st)
+
+allDirsByDepth dir depth = do
+  st <- get	
+  count <- liftIO $ dirCount dir
+  size <- liftIO $ dirSize dir
+  put ((dir, (count, size)) : st)
+  dirs <- liftIO $ lsDirs dir
+  forM_ dirs (\dir -> allDirsByDepth dir (depth - 1))
+
+ls :: FilePath -> IO [FilePath]
+ls dir = do 
+  files <- try (listDirectory dir) :: IO (Either SomeException [FilePath])
+  case files of
+    Left e -> do
+      putStrLn $ "Error: " ++ show e
+      return []
+    Right files -> return $ mapToFullPath dir files
+
+lsDirs dir = do 
+  files <- try (listDirectory dir) :: IO (Either SomeException [FilePath])
+  case files of
+    Left e -> do
+      putStrLn $ "Error: " ++ show e
+      return []
+    Right files ->  filterM doesDirectoryExist (map (\fp -> dir ++ "/" ++ fp) files)
+
+mapToFullPath dir xs = map (\x -> dir ++ "/" ++ x) xs
+
+du' :: FilePath -> Int -> IO ()
+du' path depth = do
+  result <-  runStateT (allDirsByDepth path depth) []
+  putStrLn "Counts"
+  forM_ (map (\entry -> fst entry ++ " " ++ show ( fst $ snd entry)) (snd result)) putStrLn
+  putStrLn "Sizes"
+  forM_ (map (\entry -> fst entry ++ " " ++ show ( snd $ snd entry)) (snd result)) putStrLn
+
+
+du :: FilePath -> Maybe Int -> IO ()
+du path Nothing = du' path (maxBound :: Int)
+du path (Just depth) = du' path depth
+
+main = do
+    args <- getArgs
+    putStrLn $ show $ args
+    if length args == 1 
+    then do
+      du (args !! 0) Nothing
+    else do
+      du (args !! 0) (Just (read (args !! 1)))
