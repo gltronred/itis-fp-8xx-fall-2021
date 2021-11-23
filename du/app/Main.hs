97e5f5aeabfc76dee76773d676f8f841c8ef3210
PrepConcede <tagmirs_letter@mail.ru>
2021-11-23 21:27:52 +0300

    Revert "du base"
    This reverts commit c93b5011b72f41f5848e8389cb5e9c3465238ae9.

--- du/app/Main.hs
+++ du/app/Main.hs
@@ -1,47 +1,50 @@
 {-# LANGUAGE FlexibleContexts #-}
-
 module Main where
-
+import System.Directory (listDirectory, doesDirectoryExist, getFileSize, getCurrentDirectory)
 import Control.Monad.State
-import System.Directory (doesDirectoryExist, getCurrentDirectory, getFileSize, listDirectory)
+
 
 getDirectoriesInSpecifiedFolder :: FilePath -> IO [FilePath]
 getDirectoriesInSpecifiedFolder currentPath = do
-  allFiles <- listDirectory currentPath
-  filterM doesDirectoryExist (map (\fp -> currentPath ++ "/" ++ fp) allFiles)
-
-getFileSizeStr :: FilePath -> IO String
-getFileSizeStr path = do
-  size <- getFileSize path
-  return $ path ++ "\t" ++ show size
-
-getObjectCount :: FilePath -> IO String
-getObjectCount path = do
-  v <- listDirectory path
-  return $ path ++ "\t" ++ show (length v)
+    allFiles <- listDirectory currentPath
+    filterM doesDirectoryExist (map (\fp -> currentPath ++ "/" ++ fp) allFiles)
 
 getDirectoriesByDepth :: FilePath -> Int -> StateT [FilePath] IO Bool
 getDirectoriesByDepth path 0 = do
-  modify (path :)
-  return True
+    modify (path :)
+    return True
 getDirectoriesByDepth path depth = do
-  modify (path :)
-  dirs <- liftIO $ getDirectoriesInSpecifiedFolder path
-  forM_ dirs (\dir -> getDirectoriesByDepth dir (depth -1))
-  return True
+    modify (path :)
+    dirs <- liftIO $ getDirectoriesInSpecifiedFolder path
+    forM_ dirs (\dir -> getDirectoriesByDepth dir (depth-1))
+    return True
 
-duHelper :: FilePath -> Int -> IO ()
-duHelper path depth = do
-    folders <- runStateT (getDirectoriesByDepth path depth) []
-    counts <- forM (snd folders) getObjectCount
-    sizes <- forM (snd folders) getFileSizeStr
-    putStrLn "Count:"
-    forM_ counts putStrLn
-    putStrLn "Size:"
-    forM_ sizes putStrLn
 
-du :: FilePath -> Maybe Int -> IO ()
-du path Nothing = duHelper path (maxBound:: Int)
-du path (Just depth) = duHelper path depth
+helper :: (Eq t, Num t) => FilePath -> [FilePath] -> t -> IO [FilePath]
+helper directory accum 0 = do
+    return accum
+helper directory accum depth = do
+    dirs <- getDirectoriesInSpecifiedFolder directory
+    helper (directory ++ "/" ++ head dirs) (accum ++ map (\d -> directory ++ "/" ++ d) dirs) (depth -1)
 
-main = du "." (Just 1)
+ioInsideState :: StateT [FilePath] IO Bool
+ioInsideState = do
+    v <- get
+    put ["."]
+    dirs <- liftIO $ getDirectoriesInSpecifiedFolder "."
+    put $ ["f", "f2"] ++ dirs
+    return True
+
+checkioInsideState = do
+    runStateT ioInsideState []
+
+getFileSizeString path = do
+    size <- getFileSize path
+    return $ path ++ "      " ++ show size
+main = do
+    path <- getCurrentDirectory
+    v <- runStateT (getDirectoriesByDepth path 2) []
+    sizes <- forM (snd v) getFileSizeString
+    --forM_ (snd v) (\path ->print (path ++ getFileSizeString path))
+    forM_ sizes print
+    
