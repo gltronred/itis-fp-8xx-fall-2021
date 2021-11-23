86cbe3c534821a91d42f5714d291a8c09ce544d1
PrepConcede <tagmirs_letter@mail.ru>
2021-11-23 21:28:48 +0300

    Revert "research"
    This reverts commit b9f298b2b0f7d7921d09f9e8700f03ff31f22eb4.

--- du/app/Main.hs
+++ du/app/Main.hs
@@ -1,15 +1,4 @@
 module Main where
-import Control.Monad.Writer.Lazy (WriterT, forM, filterM, MonadTrans (lift))
-import System.Directory (listDirectory, doesDirectoryExist, getFileSize, getCurrentDirectory)
 
-
-getDirectories :: FilePath -> IO [FilePath]
-getDirectories currentPath = do
-    allFiles <- listDirectory currentPath
-    filterM doesDirectoryExist allFiles
-
-main = do
-    path <- getCurrentDirectory 
-    print path
-    size <- getFileSize path
-    print size
+main :: IO ()
+main = putStrLn "Hello, Haskell!"
