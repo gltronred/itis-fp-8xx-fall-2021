bbc4b23cbadca7f13c932962f583b5d321609482
Mansur Ziiatdinov <gltronred@gmail.com>
2021-11-26 20:28:49 +0300

    Implementation of JokeAPI client

--- proj5/app/Main.hs
+++ proj5/app/Main.hs
@@ -8,33 +8,10 @@ import Data.Aeson
 import Data.Proxy
 import Data.Text (Text)
 import qualified Data.Text as T
+import Network.HTTP.Client.TLS (newTlsManager)
 import Servant.API
 import Servant.Client
-
-data Category
-  = Programming
-  | Misc
-  | Dark
-  | Pun
-  | Spooky
-  | Christmas
-  deriving (Eq,Show)
-
-type Categories = CommaSep Category
-
-data Lang = CS | DE | EN | ES | FR | PT
-  deriving (Eq,Show)
-
-data Flag
-  = Nsfw
-  | Religious
-  | Political
-  | Racist
-  | Sexist
-  | Explicit
-  deriving (Eq,Show)
-
-type Flags = CommaSep Flag
+import Web.HttpApiData
 
 newtype CommaSep a = CommaSep [a]
   deriving (Eq,Show)
@@ -61,8 +38,53 @@ instance ToHttpApiData a => ToHttpApiData (EmptyAny a) where
   toUrlPiece (EmptyAny (CommaSep [])) = "Any"
   toUrlPiece (EmptyAny t) = toUrlPiece t
 
+data Category
+  = Programming
+  | Misc
+  | Dark
+  | Pun
+  | Spooky
+  | Christmas
+  deriving (Eq,Show,Read,Bounded,Enum)
+
+instance FromHttpApiData Category where
+  parseUrlPiece = parseBoundedTextData
+instance ToHttpApiData Category where
+  toUrlPiece = T.pack . show
+
+type Categories = EmptyAny Category
+
+data Lang = Cs | De | En | Es | Fr | Pt
+  deriving (Eq,Show,Read,Bounded,Enum)
+
+instance FromHttpApiData Lang where
+  parseUrlPiece = parseBoundedTextData
+instance ToHttpApiData Lang where
+  toUrlPiece = T.toLower . T.pack . show
+
+data Flag
+  = Nsfw
+  | Religious
+  | Political
+  | Racist
+  | Sexist
+  | Explicit
+  deriving (Eq,Show,Read,Bounded,Enum)
+
+instance FromHttpApiData Flag where
+  parseUrlPiece = parseBoundedTextData
+instance ToHttpApiData Flag where
+  toUrlPiece = T.toLower . T.pack . show
+
+type Flags = CommaSep Flag
+
 data JokeType = Single | Twopart
-  deriving (Eq,Show)
+  deriving (Eq,Show,Read,Bounded,Enum)
+
+instance FromHttpApiData JokeType where
+  parseUrlPiece = parseBoundedTextData
+instance ToHttpApiData JokeType where
+  toUrlPiece = T.toLower . T.pack . show
 
 data Range = Range
   { from :: Int
@@ -77,6 +99,9 @@ instance FromHttpApiData Range where
     case ints of
       [f,t] -> pure $ Range f t
       _ -> Left "Wrong number of components in range"
+instance ToHttpApiData Range where
+  toQueryParam (Range f t) = T.concat
+    [ T.pack $ show f, "-", T.pack $ show t]
 
 type JokeApi
   = "joke"
@@ -91,14 +116,24 @@ type JokeApi
 api :: Proxy JokeApi
 api = Proxy
 
--- joke :: Categories
---      -> Maybe Lang
---      -> Maybe Flags
---      -> Maybe JokeType
---      -> Maybe Text
---      -> Maybe Range
---      -> ClientM Value
--- joke = client api
+joke :: Categories
+     -> Maybe Lang
+     -> Maybe Flags
+     -> Maybe JokeType
+     -> Maybe Text
+     -> Maybe Range
+     -> ClientM Value
+joke = client api
+
+run :: ClientM a -> IO (Either String a)
+run actions = do
+  mgr <- newTlsManager
+  let env = mkClientEnv mgr $
+        BaseUrl Https "v2.jokeapi.dev" 443 ""
+  res <- runClientM actions env
+  case res of
+    Left err -> print err >> pure (Left $ show err)
+    Right a -> pure $ Right a
 
 main :: IO ()
 main = putStrLn "Hello, Haskell!"
