9306e6bda09d3866f86a219a8a5898b537fe5ef8
Anna Fedorova <anyafed242001@gmail.com>
2021-12-04 13:33:40 +0300

    sum prime

--- proj2/app/Main.hs
+++ proj2/app/Main.hs
@@ -94,16 +94,15 @@ retryN n act = do
 
 count :: TQueue Int -> Async () -> Int -> IO Int
 count q r s = do
-  (finished, mx) <- atomically $ (,)
-    <$> pollSTM r
-    <*> tryReadTQueue q
+  mx <- atomically $ tryReadTQueue q
   case mx of
     Nothing -> do
-      -- finished <- poll r
+      finished <- poll r
       case finished of
         Nothing -> count q r s
         Just _ -> pure s
-    Just x -> count q r $! s+x
+    Just x -> count q r $! s + (if isPrime x then x else 0)
+
 
 writer :: TQueue Int -> IO ()
 writer q = do
